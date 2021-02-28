9d8382e34d52895648a6ced7db75d9a8c6eb5eba
evan bechtol <evanbechtol@gmail.com>
2021-02-28 09:41:02 -0600

    requestUtil update
    - Create method `randomNumber`

--- test/requestUtil.js
+++ test/requestUtil.js
@@ -71,4 +71,17 @@ async function removeDictionary ( endpoint, id ) {
   }
 }
 
-module.exports = { createDictionary, getValueForKey, removeDictionary };
+/**
+ * @description Generate a random number between max, and min
+ * @param max {int} Integer to represent maximum value generated number can
+ *   have
+ * @param min {int} Integer to represent minimum value generated number can
+ *   have
+ * @returns {number} Returns a randomly generated number between the max and
+ *   min provided
+ */
+function randomNumber ( max = 100, min = 0 ) {
+  return Math.floor( ( Math.random() * max ) + min );
+}
+
+module.exports = { createDictionary, getValueForKey, randomNumber, removeDictionary };
